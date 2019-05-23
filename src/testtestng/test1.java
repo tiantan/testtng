c572d2ca18ce73eb4979323d625ccfd4c56efa1b
shenyiban <707150922@qq.com>
2019-05-23 09:42:36 +0800

    文件修改

--- src/testtestng/test1.java
+++ src/testtestng/test1.java
@@ -4,14 +4,22 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
+
 public class test1 {
 	@Test
-	public void tes1(){		
+	public void tes1(){
+
+		System.setProperty("webdriver.gecko.driver","G:\\IDEAjb\\testtng\\libs\\geckodriver.exe");
+		WebDriver wd = new FirefoxDriver();
+		wd.get("http://www.baidu.com");
+		wd.close();
+		/*
 		WebDriver	driver=new FirefoxDriver();
 			driver.get("https://www.baidu.com/");;
 			driver.manage().window().maximize();
-			loginpage t=new loginpage(driver);
-			t.kw("test");
+		//	loginpage t=new loginpage(driver);
+			//t.kw("test");
 		driver.close();
+		 */
 	}
 }
