54bc39eb1726627db2fb77040ec1ddc7e922372f
tiantan <707150922@qq.com>
2019-05-24 13:38:56 +0800

    Update test1.java

--- src/testtestng/test1.java
+++ src/testtestng/test1.java
@@ -2,12 +2,12 @@ package testtestng;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-//import org.testng.annotations.Test;
+import org.testng.annotations.Test;
 
 public class test1 {
-	//@Test
-	//public void tes1(){
-	public static void main(String[] args) {
+	@Test
+	public void tes1(){
+	//public static void main(String[] args) {
 			System.setProperty("webdriver.gecko.driver","d:\\geckodriver.exe");
 			WebDriver	driver=new FirefoxDriver();
 			driver.get("https://www.baidu.com/");;
