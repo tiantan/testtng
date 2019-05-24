50c40fbbb0b39232ec325a94aeb9510243eb12a6
shenyiban <707150922@qq.com>
2019-05-24 14:20:11 +0800

    文件修改11

--- src/testtestng/test1.java
+++ src/testtestng/test1.java
@@ -7,8 +7,9 @@ import org.testng.annotations.Test;
 public class test1 {
 	@Test
 	public void tes1(){
+
 	//public static void main(String[] args) {
-			System.setProperty("webdriver.gecko.driver","d:\\geckodriver.exe");
+			System.setProperty("webdriver.gecko.driver",".\\libs\\geckodriver.exe");
 			WebDriver	driver=new FirefoxDriver();
 			driver.get("https://www.baidu.com/");;
 			driver.manage().window().maximize();
