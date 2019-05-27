6c4a888b391c73b21f68e61dc2ab5bba78a0bba3
shenyiban <707150922@qq.com>
2019-05-27 08:51:48 +0800

    文件修改11

--- src/testtestng/LoginTest.java
+++ src/testtestng/LoginTest.java
@@ -36,12 +36,10 @@ public class LoginTest extends opendriver{
         // Thread.sleep(10000);
         t.waitToDisplayed(By.id("top-nav-ess-name"));
         String zhuom=driver.findElement( By.id("top-nav-ess-name")).getText();
-        System.out.println("---------"+zhuom);
         System.out.println(zhuom);
         Assertion.flag=true;
         Assertion.verifyEquals(zhuom,"桌面2");
         Assert.assertTrue(Assertion.flag);
-
 
     }
 }
